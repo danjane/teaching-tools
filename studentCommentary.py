63f4abff8d34d78d9117a91de221da11e33910bb
danjane <jdtjane@gmail.com>
2018-10-24 10:53:27 +0200

    Basic functionality for scraping pensees file and doing basic analysis

---
+++ studentCommentary.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+import studentFunctions as sF
+
+pd.set_option('display.max_colwidth', -1)
+pensees = sF.scrape_pensees()
+
+
+def student_notes_for_latex(student):
+    notes_df = pensees[pensees['Student'].isin([student])]
+    notes_df = notes_df[['Date', 'Class', 'Info']]
+
+    str = '\\begin{minipage}[t]{\\textwidth} \n{\\small \\textbf{' + sF.classes[course][student] + '}}\n\n'
+    str += notes_df.to_latex(index=False)
+    str += '\\end{minipage}\n\n\\vspace{0.4cm} \n\n'
+
+    return str
+
+
+report = ''
+for course in sF.courses - {sF.rg_class}:
+    for student in sF.classes[course].keys():
+        report += student_notes_for_latex(student)
+
+
+report_file = sF.student_class_path.replace('COURSE.txt', 'Latex/report.tex')
+skeleton_file = sF.student_class_path.replace('COURSE.txt', 'Latex/report_skeleton.tex')
+
+with open(skeleton_file, 'r') as f:
+    latex_str = f.read()
+
+with open(report_file, 'w') as f:
+    f.write(latex_str.replace('ReportHere', report))
+
---
+++ studentFunctions.py
@@ -0,0 +1,159 @@
+import re
+import dateutil
+import numpy as np
+import pandas as pd
+
+
+# Find this year's information
+import elevesPaths
+student_class_path = elevesPaths.student_class_path
+courses = elevesPaths.courses
+rg_class = elevesPaths.rg_class
+
+
+# Define classes and pensees when loaded
+classes = {}
+
+
+def load_student_file(f):
+    f = open(f)
+    students = {}
+    for s in f.readlines():
+        student = s.strip().split(', ')
+        if len(student) > 1:
+            # Has a given name
+            name = student[1]
+        else:
+            # Take Title name
+            name = re.search(r'[A-Z][a-z].+$', student[0]).group(0)
+        students[name] = student[0]
+
+    return students
+
+
+def find_students_in_info(str, course):
+    students = []
+    for student in classes[course].keys():
+        words_re = re.compile(r'\b' + student + r'\b')
+        t = words_re.search(str)
+        if t:
+            students.append(t.group(0))
+
+    if len(students) == 0:
+        students = ['general']
+
+    return students
+
+
+def open_file(filename, open_type='r'):
+    return open(student_class_path.replace('COURSE', filename), open_type)
+
+
+# Scrape the pensees file
+def scrape_pensees():
+    with open_file('Pensees') as f:
+        pensees_file = [s.strip() for s in f.readlines()]
+
+    current_info = {}
+    pensees = pd.DataFrame()
+
+    new_day_flag = True
+    for line in pensees_file:
+        if len(line) < 1:
+            new_day_flag = True
+            continue
+
+        if new_day_flag:
+            try:
+                current_info['Date'] = [dateutil.parser.parse(line)]
+                continue
+            except ValueError as e:
+                pass
+
+        if line in courses:
+            current_info['Class'] = [line]
+        else:
+            students = find_students_in_info(line, current_info['Class'][0])
+            current_info['Info'] = [line]
+            for student in students:
+                current_info['Student'] = [student]
+                pensees = pd.concat([pensees, pd.DataFrame(current_info)])
+
+    pensees.reset_index(drop=True)
+
+    pensees['Sentiment'] = pensees.apply(lambda row: row['Info'][0] in {'+', '-'}, axis=1)
+    pensees['Positive'] = pensees.apply(lambda row: row['Info'][0] == '+', axis=1)
+
+    # Create series object of delta dates
+    date_diffs = (pensees['Date'] - max(pensees['Date'])).apply(lambda x: x.days)
+    pensees['Weight'] = date_diffs.apply(lambda x: np.exp(x / 10.))
+
+    return pensees
+
+
+def positive_comments(pensees):
+    # Should highlight students to engage with
+    print('Positive comments needed for:')
+
+    for course in courses - {rg_class}:
+        pensees_class = pensees[pensees['Class'].isin([course]) & ~pensees['Student'].isin(['general'])]
+
+        student_weights = pensees_class.groupby(['Student']).agg({'Weight': 'sum'})
+        student_weights.sort_values('Weight', inplace=True)
+
+        student_weights = student_weights.head(5)
+
+        print('\n' + course)
+        print(' '.join(student_weights.index))
+
+
+def exam_marks(xlsx_file):
+    df_exam_results = pd.read_excel(xlsx_file, sheet_name="Sheet1")
+
+    # Find the questions
+    question_count = 1
+    while not df_exam_results.columns[question_count].startswith('Unnamed'):
+        question_count += 1
+
+    # Find the student rows
+    def students():
+        idx = df_exam_results.Questions.copy().values
+
+        count = 0
+        while not idx[count] == 'Student':
+            idx[count] = False
+            count += 1
+
+        idx[count] = False
+        count += 1
+
+        while isinstance(idx[count], str):
+            idx[count] = True
+            count += 1
+
+        idx[count:] = False
+        return idx
+
+    student_idx = students()
+
+    def students_as_idx(df):
+        df.rename(columns={'Questions': 'Student'}, inplace=True)
+        df.set_index('Student', inplace=True)
+        return df
+
+    # Pull off results per question...
+    results = df_exam_results[student_idx][df_exam_results.columns[:question_count]]
+    results = students_as_idx(results)
+
+    # ... and the overall note given
+    notes = df_exam_results[student_idx][['Questions', 'Note']]
+    notes = students_as_idx(notes)
+
+    return results, notes
+
+
+# Load courses on startup
+for c in courses:
+    classes[c] = load_student_file(
+        student_class_path.replace('COURSE', c))
+
