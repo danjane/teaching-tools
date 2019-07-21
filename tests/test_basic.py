f68143b6555af59e3834c162216de26e775f47d0
danjane <jdtjane@gmail.com>
2019-07-21 16:26:01 +0200

    Added tests for loading and processing example files, and creating the latex report

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,8 @@
-import studentFunctions as sF
 import numpy as np
+import pandas as pd
+
+import studentFunctions as sF
+import studentCommentary
 
 # Tests for round_note
 
@@ -28,3 +31,46 @@ def test_round_single_note_hundredth():
     assert sF.round_note(2.22222, 0.01) == 2.22
 
 
+# Tests for loading and processing
+
+def test_loading_students():
+    expected = {
+        'CRAMER Gabriel': 'Gabriel',
+        'CURIE Marie': 'Marie',
+        'EINSTEIN Albert': 'Albert',
+        'FEYNMAN Richard': 'Dick'}
+    assert sF.load_student_file('./example_files/example_class.txt') == expected
+
+
+def test_scrape_pensees():
+    example_class = sF.load_student_file('./example_files/example_class.txt')  # TODO(dan) where should this be?
+
+    sF.courses = ['example_class']
+    sF.student_class_path = './example_files/COURSE.txt'
+    sF.courses = ['example_class']
+    sF.rg_class = None
+    sF.classes = {'example_class': example_class}
+
+    expected_pensees = pd.read_csv('example_pensees_scrape.csv')
+
+    example_pensees = sF.scrape_pensees()
+    example_pensees.reset_index(inplace=True)
+    example_pensees.drop(columns=['index'], inplace=True)
+
+    for col in expected_pensees:
+        if col in ['Date', 'Weight']:
+            continue
+        assert (example_pensees[col] == expected_pensees[col]).all()  # TODO(dan) why is all not happy?
+
+
+def test_report():
+    example_class = sF.load_student_file('./example_files/example_class.txt')
+
+    sF.courses = ['example_class']
+    sF.student_class_path = './example_files/COURSE.txt'
+    sF.courses = ['example_class']
+    sF.rg_class = None
+    sF.classes = {'example_class': example_class}
+
+    studentCommentary.pensees = sF.scrape_pensees()  # TODO(dan) how should I include this test text in the .py file?
+    assert studentCommentary.student_notes_for_latex('EINSTEIN Albert', 'example_class') == "\\begin{minipage}[t][\\textheight]{\\textwidth} \n{\\large \\textbf{EINSTEIN Albert}}\\hfill example_class\n\\vspace{1cm}\n\n\\begin{tabular}{ll}\n\\toprule\n      Date &                                    Info \\\\\n\\midrule\n2018-08-27 &  -Albert s'ennui en classe \\\\\n2018-08-28 &  +Albert interagis bien avec les autres \\\\\n\\bottomrule\n\\end{tabular}\n\n\\vfill\n\\begin{minipage}[t]{0.5\\textwidth}\n\\includegraphics[width = 0.8\\textwidth]{EINSTEINAlbert_Sentiment.png}\\end{minipage}\n\\begin{minipage}[t]{0.5\\textwidth}\n\\includegraphics[width = 0.8\\textwidth]{EINSTEINAlbert_Notes.png}\\end{minipage}\n\n\\end{minipage}\n\n\\newpage \n\n"
